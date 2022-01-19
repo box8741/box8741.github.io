cf6724eb6b26f7351c220c357091410b784a2200
box8741 <box8741@gmail.com>
2022-01-19 20:25:38 +0900

    add : 더미데이터 실 데이터로 변경 작업

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -1,15 +1,11 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
-import PostItem from './PostItem'
+import PostItem from 'components/Main/PostItem'
 
-const POST_ITEM_DATA = {
-  title: 'Post Item Title',
-  date: '2020.01.29.',
-  categories: ['Web', 'Frontend', 'Testing'],
-  summary:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
-  thumbnail: 'https://www.smlounge.co.kr/upload/living/article/202012/thumb/46702-435751-sampleM.jpg',
-  link: 'https://www.google.co.kr/',
+import { PostListItemType } from 'types/PostItem.types'
+
+type PostListProps = {
+  posts: PostListItemType[]
 }
 
 const PostListWrapper = styled.div`
@@ -27,13 +23,12 @@ const PostListWrapper = styled.div`
   }
 `
 
-const PostList: FunctionComponent = () => {
+const PostList: FunctionComponent<PostListProps> = ({ posts }) => {
   return (
     <PostListWrapper>
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
+      {posts.map(({ node: { id, frontmatter } }) => (
+        <PostItem {...frontmatter} key={id} link="https://www.google.co.kr/" />
+      ))}
     </PostListWrapper>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
+import { graphql } from 'gatsby'
 import GlobalStyle from 'components/Common/GlobalStyle'
 import Footer from 'components/Common/Footer'
-import Introduction from 'components/Main/introduction'
+import Introduction from 'components/Main/Introduction'
 import CategoryList from 'components/Main/CategoryList'
 import PostList from 'components/Main/PostList'
+
+import { PostListItemType } from 'types/PostItem.types'
+
+type IndexPageProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostListItemType[]
+    }
+  }
+}
 
 const CATEGORY_LIST = {
   All: 5,
@@ -18,16 +29,41 @@ const Container = styled.div`
   height: 100vh;
 `
 
-const IndexPage: FunctionComponent = () => {
+const IndexPage: FunctionComponent<IndexPageProps> = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
   return (
     <Container>
       <GlobalStyle />
       <Introduction />
       <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
-      <PostList />
+      <PostList posts={edges} />
       <Footer />
     </Container>
   )
 }
 
 export default IndexPage
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`
