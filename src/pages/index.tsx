e952659f2a548531d5e1303367ab8aae46f3ab4e
box8741 <box8741@gmail.com>
2022-01-19 17:38:57 +0900

    add : 메인 UI 작업 진행중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobalStyle'
-import Introduction from 'components/Main/introduction'
 import Footer from 'components/Common/Footer'
+import Introduction from 'components/Main/introduction'
+import CategoryList from 'components/Main/CategoryList'
+import PostList from 'components/Main/PostList'
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
 
 const Container = styled.div`
   display: flex;
@@ -10,11 +18,13 @@ const Container = styled.div`
   height: 100vh;
 `
 
-const IndexPage = () => {
+const IndexPage: FunctionComponent = () => {
   return (
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <PostList />
       <Footer />
     </Container>
   )
