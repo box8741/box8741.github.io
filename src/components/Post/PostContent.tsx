36a201fd7e2793bc97e347dd198c3f79de8613a4
box8741 <box8741@gmail.com>
2022-01-21 18:18:23 +0900

    add : Post 페이지 반응형 작업

--- src/components/Post/PostContent.tsx
+++ src/components/Post/PostContent.tsx
@@ -6,7 +6,6 @@ interface PostContentProps {
 }
 
 const MarkdownRenderer = styled.div`
-  // Renderer Style
   display: flex;
   flex-direction: column;
   width: 768px;
@@ -14,17 +13,14 @@ const MarkdownRenderer = styled.div`
   padding: 100px 0;
   word-break: break-all;
 
-  // Markdown Style
   line-height: 1.8;
   font-size: 16px;
   font-weight: 400;
 
-  // Apply Padding Attribute to All Elements
   p {
     padding: 3px 0;
   }
 
-  // Adjust Heading Element Style
   h1,
   h2,
   h3 {
@@ -56,7 +52,6 @@ const MarkdownRenderer = styled.div`
     font-size: 20px;
   }
 
-  // Adjust Quotation Element Style
   blockquote {
     margin: 30px 0;
     padding: 5px 15px;
@@ -64,26 +59,22 @@ const MarkdownRenderer = styled.div`
     font-weight: 800;
   }
 
-  // Adjust List Element Style
   ol,
   ul {
     margin-left: 20px;
     padding: 30px 0;
   }
 
-  // Adjust Horizontal Rule style
   hr {
     border: 1px solid #000000;
     margin: 100px 0;
   }
 
-  // Adjust Link Element Style
   a {
     color: #4263eb;
     text-decoration: underline;
   }
 
-  // Adjust Code Style
   pre[class*='language-'] {
     margin: 30px 0;
     padding: 15px;
@@ -98,6 +89,33 @@ const MarkdownRenderer = styled.div`
   code[class*='language-'],
   pre[class*='language-'] {
     tab-size: 2;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 80px 20px;
+    line-height: 1.6;
+    font-size: 14px;
+
+    h1 {
+      font-size: 23px;
+    }
+
+    h2 {
+      font-size: 20px;
+    }
+
+    h3 {
+      font-size: 17px;
+    }
+
+    img {
+      width: 100%;
+    }
+
+    hr {
+      margin: 50px 0;
+    }
   }
 `
 
--- src/components/Post/PostHead.tsx
+++ src/components/Post/PostHead.tsx
@@ -11,6 +11,10 @@ const PostHeadWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 400px;
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const BackgroundImage = styled((props: GatsbyImageProps) => (
@@ -21,6 +25,10 @@ const BackgroundImage = styled((props: GatsbyImageProps) => (
   height: 400px;
   object-fit: cover;
   filter: brightness(0.25);
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const PostHead: FunctionComponent<PostHeadProps> = ({ thumbnail, title, date, categories }) => {
