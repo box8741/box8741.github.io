c3fc047aa37681dcc05b65f9a5fc943109949955
box8741 <box8741@gmail.com>
2022-01-21 18:00:55 +0900

    add : Utterances 위젯 추가 작업

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 import Template from 'components/Common/Template'
 import PostHead from 'components/Post/PostHead'
 import PostContent from 'components/Post/PostContent'
+import CommentWidget from 'components/Post/CommonWidget'
 
 import { PostFrontmatterType } from 'types/PostItem.types'
 
@@ -45,6 +46,7 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = ({
     <Template>
       <PostHead title={title} date={date} categories={categories} thumbnail={gatsbyImageData} />
       <PostContent html={html} />
+      <CommentWidget />
     </Template>
   )
 }
