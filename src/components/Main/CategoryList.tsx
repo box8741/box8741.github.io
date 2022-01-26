c30c58dd6d01e5eef77c4daec6bfd541728d8d87
box8741 <box8741@gmail.com>
2022-01-26 18:25:57 +0900

    style : 카테고리 글씨 사이즈 수정

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -29,16 +29,12 @@ const CategoryListWrapper = styled.div`
 const CategoryItem = styled(({ active, ...props }: CategoryItemProps) => <Link {...props} />)`
   margin-right: 20px;
   padding: 5px 0;
-  font-size: 18px;
+  font-size: 15px;
   font-weight: ${({ active }) => (active ? '800' : '400')};
   cursor: pointer;
 
   &:last-of-type {
     margin-right: 0;
-  }
-
-  @media (max-width: 768px) {
-    font-size: 15px;
   }
 `
 
