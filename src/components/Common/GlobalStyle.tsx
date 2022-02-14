c0cc528587884617e36d05d4a2e10808ef550ccc
box8741 <box8741@gmail.com>
2022-02-14 16:31:58 +0900

    style : scrollbar 스타일 변경

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -13,13 +13,26 @@ const defaultStyle = css`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'GowunDodum-Regular', serif;
+    font-family: 'GowunDodum-Regular';
   }
 
   html,
   body,
   #___gatsby {
     height: 100%;
+  }
+
+  body::-webkit-scrollbar {
+    width: 12px;
+    background: gray;
+  }
+  body::-webkit-scrollbar-thumb {
+    background: #333333;
+    border-radius: 10px;
+    background-clip: padding-box;
+    border: 2px solid transparent;
+  }
+  body::-webkit-scrollbar-track {
   }
 
   a,
