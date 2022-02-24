3b88fdeb608bc7d7745d9262a6a9d9acbc08b754
box8741 <box8741@gmail.com>
2022-02-24 21:21:16 +0900

    refactor : 사용하지 않는 변수 제거

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,4 @@
 import React, { useMemo, FunctionComponent } from 'react'
-import { IGatsbyImageData } from 'gatsby-plugin-image'
 import { graphql, PageProps } from 'gatsby'
 import queryString, { ParsedQuery } from 'query-string'
 import Layout from 'components/Common/Layout'
